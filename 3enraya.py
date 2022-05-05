1d74a991e8dcdb0d65ea1f1bcb8e1ba24648c67d
Yaroslav Holovatskyy <holovatskyyy@gmail.com>
2022-05-05 16:57:55 +0200

    v2

--- 3enraya.py
+++ 3enraya.py
@@ -1,10 +1,10 @@
-CELDAS = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+CELDAS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
 X, O, BLANCO = 'X', 'O', ' '
 
 
 def main():
-    print('Bienvenido a 3 en raya!')
-    tableroJuego = obtenerTableroVacio()  # Crea el tablero vacío de 3 en raya
+    print('Bienvenido a 4 en raya!')
+    tableroJuego = obtenerTableroVacio()  # Crea el tablero vacío de 4 en raya
     jugadorActual, jugadorSiguiente = X, O  # X primero, O siguiente
 
     while True:  # Bucle principal del juego
@@ -15,7 +15,7 @@ def main():
         movimiento = None
         while not esCeldaValida(tableroJuego, movimiento):
             print('')
-            print('Cuál es el movimiento de {}? (1-9)'.format(jugadorActual))
+            print('Cuál es el movimiento de {}? (1-16)'.format(jugadorActual))
             movimiento = input('> ')
 
         tableroJuego[movimiento] = jugadorActual
@@ -39,12 +39,14 @@ def main():
 
 
 def obtenerTableroVacio():
-    """Crea un tablero vacío para la partida de 3 en raya."""
-    # Mapeado : 1|2|3
-    #           -+-+-
-    #           4|5|6
-    #           -+-+-
-    #           7|8|9
+    """Crea un tablero vacío para la partida de 4 en raya."""
+    # Mapeado :  1|  2|  3|  4
+    #            -+-+-+-+-+-+-
+    #            5|  6|  7|  8
+    #            -+-+-+-+-+-+-
+    #            9| 10| 11| 12
+    #            -+-+-+-+-+-+-
+    #           13| 14| 15| 16
     tablero = {}
     for celda in CELDAS:
         tablero[celda] = BLANCO  # Todas las celdas del tablero se inicializan a BLANCO
@@ -54,13 +56,16 @@ def obtenerTableroVacio():
 def obtenerStrTablero(tablero):
     """Devuelve el tablero formateado como cadena de texto."""
     return '''
-      {}|{}|{}  1 2 3
-      -+-+-
-      {}|{}|{}  4 5 6
-      -+-+-
-      {}|{}|{}  7 8 9'''.format(tablero['1'], tablero['2'], tablero['3'],
-                                tablero['4'], tablero['5'], tablero['6'],
-                                tablero['7'], tablero['8'], tablero['9'])
+      |{}|{}|{}|{}|  1  2  3  4
+      -+-+-+-+-
+      |{}|{}|{}|{}|  5  6  7  8
+      -+-+-+-+-
+      |{}|{}|{}|{}|  9 10 11 12
+      -+-+-+-+-
+      |{}|{}|{}|{}| 13 14 15 16'''.format(tablero['1'], tablero['2'], tablero['3'], tablero['4'],
+                                          tablero['5'], tablero['6'], tablero['7'], tablero['8'],
+                                          tablero['9'], tablero['10'], tablero['11'], tablero['12'],
+                                          tablero['13'], tablero['14'], tablero['15'], tablero['16'])
 
 
 def esCeldaValida(tablero, celda):
@@ -72,15 +77,17 @@ def esGanador(tablero, jugador):
     """Devuelve True si el jugador es ganador."""
     # Se utilizan variables cortas para mejorar la legibilidad
     b, p = tablero, jugador
-    # Busca el 3 en raya en las 3 filas, 3 columnas y las 2 diagonales
-    return ((b['1'] == b['2'] == b['3'] == p) or  # Horizontal arriba
-            (b['4'] == b['5'] == b['6'] == p) or  # Horizontal medio
-            (b['7'] == b['8'] == b['9'] == p) or  # Horizontal abajo
-            (b['1'] == b['4'] == b['7'] == p) or  # Vertical izquierda
-            (b['2'] == b['5'] == b['8'] == p) or  # Vertical centro
-            (b['3'] == b['6'] == b['9'] == p) or  # Vertical derecha
-            (b['3'] == b['5'] == b['7'] == p) or  # Diagonal
-            (b['1'] == b['5'] == b['9'] == p))  # Diagonal
+    # Busca el 4 en raya en las 4 filas, 4 columnas y las 2 diagonales
+    return ((b['1'] == b['2'] == b['3'] == b['4'] == p) or  # Horizontal arriba
+            (b['5'] == b['6'] == b['7'] == b['8'] == p) or  # Horizontal medio
+            (b['9'] == b['10'] == b['11'] == b['12'] == p) or  # Horizontal medio 2
+            (b['13'] == b['14'] == b['15'] == b['16'] == p) or  # Horizontal abajo
+            (b['1'] == b['5'] == b['9'] == b['13'] == p) or  # Vertical izquierda
+            (b['2'] == b['6'] == b['10'] == b['14'] == p) or  # Vertical centro 1
+            (b['3'] == b['7'] == b['11'] == b['15'] == p) or  # Vertical centro 2
+            (b['4'] == b['8'] == b['12'] == b['16'] == p) or  # Vertical derecha
+            (b['4'] == b['7'] == b['10'] == b['13'] == p) or  # Diagonal 1
+            (b['1'] == b['6'] == b['11'] == b1['16'] == p))  # Diagonal 2
 
 
 def tableroLleno(tablero):
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# MAFF
+# Y.H.H.
 
 CFGS Desenrotllament d'Aplicacions Web
 Entorns de Desenrotllament
 
-## Programa de 3 en raya
+## Programa de 4 en raya
 
 Programa exemple per activitat de Github amb Python
 
